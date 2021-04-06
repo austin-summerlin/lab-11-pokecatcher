5d8b51fc347b01e802ed037ec1f5107f5a309f97
Austin Summerlin <summerlin.austin@gmail.com>
2021-04-06 13:54:34 -0700

    fix pathing issue

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -57,7 +57,7 @@ export function capturePokemon(pokemon) {
 
     if (captured === 11) {
         button.disabled = true;
-        window.location.href = '../results/index.html';
+        window.location.href = '../../index.html';
         alert('Congratulations! You Have Caught 10 Pokemon. Please See Profesor Oak for Further Instructions.');
     }
 
